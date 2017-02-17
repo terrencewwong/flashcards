4fc2383cc9b6a71f609705dd2ab9f3e83f0f9d20
Terrence Wong <wongterrencew@gmail.com>
2017-02-17 23:30:56 +0100

    fix tabs

--- server/plugins/flashcard-service/models/store.js
+++ server/plugins/flashcard-service/models/store.js
@@ -1,31 +1,31 @@
 const shuffle = require('knuth-shuffle').knuthShuffle
 
 class Store {
-	constructor () {
+  constructor () {
     this.storage = initializeStorage()
-		this.pointer = 0
-	}
+    this.pointer = 0
+  }
 
-	getNextCard () {
-		const item = this.storage.getItemSync(this.pointer.toString())
-		this.pointer = this.pointer + 1
-		return item
-	}
+  getNextCard () {
+    const item = this.storage.getItemSync(this.pointer.toString())
+    this.pointer = this.pointer + 1
+    return item
+  }
 
-	clear () {
-		this.storage.clearSync()
-	}
+  clear () {
+    this.storage.clearSync()
+  }
 }
 
 function initializeStorage () {
-	const storage = require('node-persist')
-	storage.initSync()
+  const storage = require('node-persist')
+  storage.initSync()
 
   // don't save reference to all.json so we don't save it in memory
-	shuffle(require('../../../../cards/spanish-vocabulary/all.json'))
+  shuffle(require('../../../../cards/spanish-vocabulary/all.json'))
     .forEach((word, index) => {
-		  storage.setItemSync(index.toString(), word)
-	  })
+      storage.setItemSync(index.toString(), word)
+    })
 
   return storage
 }
