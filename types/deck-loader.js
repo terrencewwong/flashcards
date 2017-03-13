f7eaf09bed52e05712bea309c6ad93bf816a1543
Terrence Wong <wongterrencew@gmail.com>
2017-03-13 16:35:43 +0100

    feat: add method to instantiate learning strategy from deck name

--- types/deck-loader.js
+++ types/deck-loader.js
@@ -1,5 +1,5 @@
 declare interface DeckLoader {
-  isLoaded(name: string): boolean;
-  setLoaded(name: string, state: boolean): void;
-  load(name: string): any[];
+  isLoaded(): boolean;
+  setLoaded(state: boolean): void;
+  load(): any[];
 }
