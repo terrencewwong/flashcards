2d8b38040c1aac8a501308362b46b2e7db9d0a13
Terrence Wong <wongterrencew@gmail.com>
2017-12-29 09:02:39 +0100

    wip

--- bin/cmds/practice.js
+++ bin/cmds/practice.js
@@ -1,6 +1,13 @@
 // @flow
+import Wreck from 'wreck'
+
 module.exports = {
   command: 'practice <deck>',
   desc: 'Practice flashcards',
-  handler: ({ deck }: { deck: string }) => console.log(deck)
+  handler: async ({ deck }: { deck: string }) => {
+    const { res, payload }= await Wreck.get('http://localhost:8000/next-card', {
+      json: true
+    })
+    console.log(payload)
+  }
 }
